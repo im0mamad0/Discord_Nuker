e221a16824f274505b421ecdb449cb4d951798b6
im0mamad0 <162912872+im0mamad0@users.noreply.github.com>
2024-03-15 21:30:47 +0330

    File

---
+++ install.py
@@ -0,0 +1,21 @@
+import os
+
+
+os.system('pip install requests')
+os.system('pip install colored')
+os.system('pip install pystyle')
+os.system('pip install datetime')
+os.system('pip install keyboard')
+os.system('pip install tkinter')
+os.system('pip install tls_client')
+os.system('pip install threading')
+os.system('pip install easygui')
+os.system('pip install colorama')
+os.system('pip install pynput')
+os.system('pip install websocket')
+os.system('pip install fake_useragent')
+os.system('pip install httpx')
+os.system('pip install emoji')
+os.system('pip install bs4')
+os.system('pip install discum==1.1.0')
+os.system('pip install discord')
